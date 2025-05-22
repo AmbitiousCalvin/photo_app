5b002cc48b047a33436a79e5a5bded00020f8e0c
AmbitiousCalvin <190220508+AmbitiousCalvin@users.noreply.github.com>
2025-05-22 16:08:48 +0630

    Added Masonary Photo Grid and improved Slider Component

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,10 @@
+import HeroSection from "../components/HeroSection.jsx";
+import { Button, Icon } from "../components/Buttons.jsx";
+
+function HomePage() {
+	return (
+		<section>
+			<HeroSection></HeroSection>
+		</section>
+	);
+}
