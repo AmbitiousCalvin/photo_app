ca1058312fb6270f9cc221abd66fcba1ab30664e
AmbitiousCalvin <190220508+AmbitiousCalvin@users.noreply.github.com>
2025-05-24 06:57:14 +0800

    Added homepage to pacakge.json

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/photo_app/",
+	base: "/photo_app",
 	plugins: [react(), tailwindcss()],
 });
